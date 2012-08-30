5b94f8848a0bfaee3462d32b47283d75f023c02f
indapa <indapa@gmail.com>
2012-08-30 16:39:45 -0400

    added overlap option

--- bed_split.py
+++ bed_split.py
@@ -5,7 +5,7 @@ import string
 import re
 from optparse import OptionParser
 
-def returnIntervals(bedfh,size):
+def returnIntervals(bedfh,size,overlap):
     regions=[]
     region_size=size
     for line in bedfh:
@@ -17,7 +17,7 @@ def returnIntervals(bedfh,size):
         chrom_length = int(fields[2])
         region_start = 0
         
-        while region_start < chrom_length:
+        while region_start < chrom_length-overlap:
             start = region_start
             end = region_start + region_size
             if end > chrom_length:
@@ -25,7 +25,7 @@ def returnIntervals(bedfh,size):
             region_string = chrom_name + "\t" + str(region_start) + "\t" + str(end)
             regions.append(region_string)
 
-            region_start = end
+            region_start = end - overlap
 
     return regions
 
@@ -33,13 +33,14 @@ def main():
     usage = "usage: %prog [options] arg"
     parser = OptionParser(usage)
     parser.add_option("--size", type="int", dest="size", help="window size to split the bed file into, default is 10000", default=10000)
+    parser.add_option("--overlap", type="int", dest="overlap", help="window overlap size , default is 5000", default=5000)
 
     (options, args)=parser.parse_args()
 
 
     file=args[0]
     fh=open(file,'r')
-    regions=returnIntervals(fh, options.size)
+    regions=returnIntervals(fh, options.size,options.overlap)
 
     for r in regions:
         print r
