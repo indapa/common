28dfb9f0382c6d027db1c99e8d1230691628afde
Amit Indap <indapa@gmail.com>
2013-04-23 10:43:59 -0400

    initial commit interleave-fastq.py

--- interleave-fastq.py
+++ interleave-fastq.py
@@ -3,28 +3,38 @@ import sys
 import os
 import string
 import re
+import itertools
 from optparse import OptionParser
+from common import yieldFastqRecord
 import gzip
-from common import *
 
 def main():
-    usage = "usage: %prog [options]"
-    parser = OptionParser(usage)
-    parser.add_option("--forward", type="string", dest="forward", help="forward fastq gzipped file")
-    parser.add_option("--reverse", type="string", dest="reverse", help="reverse fastq gzipped file")
-
-    (options, args)=parser.parse_args()
-    fh1=gzip.open(options.forward,'rb')
-    fh2=gzip.open(options.reverse,'rb')
     
-    fq1_gen=yieldFastqRecord(fh1)
-    fq2_gen=yieldFastqRecord(fh2)
+    """ Interleave records of fastq files. For example if given file1.fq and file2.fq
+        the program will print fastq record from file1 followed by fastq record from file2,
+        alternating between to two files till exhausted. Note this assumes that the
+        fastq records have the same number of records. The code here is adapted from
+        this biostars post: http://www.biostars.org/p/67246/#67556 """
+    
+    usage = "usage: %prog [options] file1.fastq file2.fastq ... fileN.fastq"
+    parser = OptionParser(usage)
+    
+    parser.add_option("--gunzip", action="store_true", dest="gunzip", default=False,  help="fastq files are uncompressed")
+    (options, args)=parser.parse_args()
 
-    for r1, r2 in itertools.izip(fq1_gen, fq2_gen):
-        print r1
-        print r2
-
-
-
+    
+    fastqfiles=args[0:]
+    fqfilehandles=[]
+    
+    if options.gunzip == True:
+        fqfilehandles=list(itertools.imap(open, fastqfiles)) #list of filehandles for fastq files
+    else:
+        fqfilehandles=list(itertools.imap(gzip.open, fastqfiles))
+    
+    # interleave the fastas with izip and chain the results. this is all lazy
+    fastq_records = itertools.chain.from_iterable(itertools.izip(*[yieldFastqRecord(fh) for fh in fqfilehandles]))
+    
+    for header,seq,qual in fastq_records:
+        print "\n".join([header,seq,"+",qual])
 if __name__ == "__main__":
     main()
