7d184c38664686a25ac3e9f57b4472941c3ae329
Amit Indap <indapa@gmail.com>
2013-04-28 21:14:06 -0400

    fixed error in sorting by number

--- vcf-sort.sh
+++ vcf-sort.sh
@@ -2,4 +2,4 @@
 
 # Sort VCF file keeping the header. The head command is for performance. 
 #adapted from here: http://vcftools.sourceforge.net/docs.html#one-liners
-(cat $1  | head -n4| grep ^\#; cat $1  | grep -v ^\# | sort -k1,1d -k2,2n;)  
+(cat $1  |  grep ^\#; cat $1  | grep -v ^\# | sort -k1,1n -k2,2n;)  
