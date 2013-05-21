92144f4b6d7907692077a65b7e2bef4c63def253
Amit Indap <indapa@gmail.com>
2013-05-21 15:47:06 -0400

    removed --h option; causing off by one error when deciding to print first samrecord; deal with later

--- get_random_subset_PE_sam.py
+++ get_random_subset_PE_sam.py
@@ -12,7 +12,7 @@ def main():
     usage = "usage: %prog [options] readname_sorted.sam"
     parser=OptionParser(usage)
     parser.add_option("--N", type="int", default=100, dest="N", help="number of records to randomly select")
-    parser.add_option("--h", action="store_true", dest="header",  default=False, help="set if header included in samfile")
+    
     (options,args)=parser.parse_args()
     
     samfilename=args[0]
@@ -21,15 +21,7 @@ def main():
     subfh=open(basename+".subset.sam", "w")
     
     samfh=open(samfilename, 'r')
-    if options.header==True:
-        inheader=True
-        while (inheader):
-            headerline=samfh.readline()
-            if headerline.startswith('@'):
-                subfh.write(headerline)
-            else: 
-                break
-    
+ 
     records1 = sum(1 for _ in open(samfilename)) / 2
     rand_records=sorted([random.randint(0, records1 - 1) for _ in xrange(options.N)])
     
